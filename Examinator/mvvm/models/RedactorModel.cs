435b3c3a5a34015baa98fc7b26d79e9dd39c8bf4
Woipot <frostik0409@gmail.com>
2019-05-21 16:05:35 +0300

    finish creating test header edit form

--- Examinator/mvvm/models/RedactorModel.cs
+++ Examinator/mvvm/models/RedactorModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using DevExpress.Mvvm;
 using Examinator.mvvm.models.subModels;
 
@@ -11,11 +12,64 @@ namespace Examinator.mvvm.models
 
         public ObservableCollection<QuestionModel> Questions => _testModel?.Questions;
 
+        public TestModel TestModel => _testModel;
+
+        public string FullTime
+        {
+            get
+            {
+                if (_testModel == null)
+                    return "0 мин";
+                var total =  _testModel.MinutsToTest * Math.Min(_testModel.QuestionsInTest, _testModel.Questions.Count);
+                int seconds = total % 60;
+                int minutes = total / 60;
+
+                return $"{minutes} мин :  {seconds} сек";
+            }
+        }
+
+        public int MinutToTest
+        {
+            get
+            {
+                return _testModel != null ? _testModel.MinutsToTest : 0; }
+
+            set
+            {
+                if (_testModel != null)
+                {
+                    _testModel.MinutsToTest = value;
+                    RaisePropertiesChanged("FullTime");
+                }
+            }
+        }
+
+        public int QuestionsInTestCount
+        {
+            get
+            {
+                return _testModel != null ? _testModel.QuestionsInTest : 0;
+            }
+
+            set
+            {
+                if (_testModel != null)
+                {
+                    _testModel.QuestionsInTest = value;
+                    RaisePropertiesChanged("FullTime");
+                }
+            }
+        }
+
         public void SetData(TestModel testModel, PreloadedTestInfo preloadedInfo)
         {
             _testModel = testModel;
             _info = preloadedInfo;
             RaisePropertiesChanged("Questions");
+            RaisePropertiesChanged("TestModel");
+            RaisePropertiesChanged("FullTime");
+            RaisePropertiesChanged("MinutToTest");
+            RaisePropertiesChanged("QuestionsInTestCount");
         }
 
 
